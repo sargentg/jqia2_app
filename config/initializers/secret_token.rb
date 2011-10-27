0924423b412cdebbd793dd6e3a410dcedfe1cca8
George Sargent <sargentg@uww.edu>
2011-10-26 21:55:31 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jqia2App::Application.config.secret_token = '9a1f0335ba8d6f899abe0beda476f17f4bc22422c1abf871ad0889e9a0d2d12e0135f62bbd561053f99fc0f701ea3a94b9581df09a96dc665694aaeb5d918dc2'
